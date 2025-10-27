4099b0f71914794bda9c3da5703be132299e577e
dariahryn <Daria.Hryn@student.gildeopleidingen.nl>
2025-10-27 12:43:20 +0100

    Update tafelkaart.php

---
+++ tafelkaart.php
@@ -0,0 +1,20 @@
+<?php
+
+// Declaration of strings
+$var1 = "Geeksforgeeks";
+$var2 = "Priyank";
+ 
+// Display HTML table using PHP echo
+// Use $variable_name to use a string
+// in code
+echo "<table border=1 cellspacing=1 cellpadding=1>
+    <tr> 
+       <td><font color=green>$var1</font></td>
+       <td>is best learning platform</td>
+    </tr>
+    <tr> 
+        <td><font color=green>Written by</font></td>
+        <td>$var2</td>
+    </tr>
+</table>";
+?>
